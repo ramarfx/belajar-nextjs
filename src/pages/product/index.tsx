44e0cbfaf3e9dd538549fc6bdf7732dbdd4af457
ramarfx <ramagamiing33@gmail.com>
2024-03-09 17:13:51 +0700

    api & koneksi database firebase

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,20 +1,39 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
+type ProductType = {
+  id: number;
+  name: string;
+  price: number;
+  size: string;
+};
+
 const ProductPage = () => {
-  const [isLogin, setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
+  const [products, setProducts] = useState([]);
   const { push } = useRouter();
 
   useEffect(() => {
     if (!isLogin) {
       push("/auth/login");
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    fetch("/api/product")
+      .then((res) => res.json())
+      .then((res) => {
+        setProducts(res.data);
+      });
   }, []);
 
   return (
     <div>
       <p>Product Page</p>
+      {products.map((product: ProductType) => {
+        return <div key={product.id}>{product.name}</div>;
+      })}
     </div>
   );
 };
