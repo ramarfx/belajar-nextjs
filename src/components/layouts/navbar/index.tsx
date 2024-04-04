e90449b0ffdaf45707bd589132a3b1e2abf4b79f
ramarfx <ramagamiing33@gmail.com>
2024-04-04 13:50:43 +0700

    setup next-auth

--- src/components/layouts/navbar/index.tsx
+++ src/components/layouts/navbar/index.tsx
@@ -1,9 +1,20 @@
+import { signIn, signOut, useSession } from "next-auth/react";
 import styles from "./navbar.module.css";
 
 const Navbar = () => {
+  const { data }: any = useSession();
+
   return (
     <div className={styles.navbar}>
       <div>navbar</div>
+      <div>
+        {data && data.user.fullname}
+        {data ? (
+          <button className={styles.button} onClick={() => signOut()}>Sign out</button>
+        ) : (
+          <button className={styles.button} onClick={() => signIn()}>Sign In</button>
+        )}
+      </div>
     </div>
   );
 };
